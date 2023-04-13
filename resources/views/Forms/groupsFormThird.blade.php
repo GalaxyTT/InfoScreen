30e38ad6f189f6813a45f53124e6323b9436b836
Felix Schinagl <f.schinagl@hotmail.com>
2023-04-13 10:12:54 +0200

    -

--- resources/views/Forms/groupsFormThird.blade.php
+++ resources/views/Forms/groupsFormThird.blade.php
@@ -11,15 +11,13 @@
         </style>
     </head>
     <body class="antialiased h-screen w-screen flex justify-center items-center" style="background-image: url('/bg/gplay.png')">
-        <div class="h-auto bg-gray-100 border-2 rounded-lg w-2/4 flex justify-center">
+        <div class="h-auto bg-gray-100 border-2 rounded-lg w-auto flex justify-center">
             <div><form action="{{route('processForm')}}" method="post">
                 <input type="hidden" name="groupName" value="{{$groupName}}">
                 <input type="hidden" name="classId" value="{{$classId}}">
                 <input type="hidden" name="students" value="{{$students}}">
-                <div calss="flex justify-center">
-                    <button class="text-white bg-gray-800 hover:bg-gray-700 focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-lg text-sm w-auto px-10 py-2 my-4" onclick="createNew();" type="button">Neues Datum</button>
-                </div>
-                <div id="dateContainer" class="grid grid-flow-col auto-cols-max mx-4 gap-2">
+                <div class="flex justify-center"><div><button class="text-white bg-gray-800 hover:bg-gray-700 focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-lg text-sm w-auto px-10 py-2 my-4" onclick="createNew();" type="button">Neues Datum</button></div></div>
+                <div id="dateContainer" class="grid gird-cols-3 mx-4 gap-2">
                     <div id="singleDate" class="p-4 border-2 rounded-lg">
                         <input type="date" name="dates[]" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
                             <select name="teachers[]" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-gray-500 focus:border-gray-500 block p-4 my-4">
@@ -36,7 +34,7 @@
                         <button type="button" id="i" class="w-full text-white bg-gray-800 hover:bg-gray-700 focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-lg text-sm pt-2 pb-2" onclick="this.parentNode.remove()">Entfernen</button>
                     </div>
                 </div>                
-                <input class="text-white bg-gray-800 hover:bg-gray-700 focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-lg text-sm w-auto p-2 my-4" type="submit">
+                <div class="flex justify-center"><div><input class="text-white bg-gray-800 hover:bg-gray-700 focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-lg text-sm w-auto p-2 my-4" type="submit"></div></div>
             </form></div>
         </div>
     </body>
